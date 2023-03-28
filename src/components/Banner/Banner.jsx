a349b563b33c272e20f2e4189bceb1fc5c5f17c4
Jobayer <jobayerrahman0220@gmail.com>
2023-03-28 14:22:59 +0600

    responsive for medium and small device

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 
+import Button from "../UI/Button";
 import classes from "./Banner.module.css";
 import bannerImg from "../../imgs/banner.jpg";
 
 const Banner = () => {
   return (
-    <div className={classes.banner_container}>
-      <div className={classes.banner_content}>
-        <h1 className={classes.title}>New Collection For Fall</h1>
-        <p className={classes.para}>
-          Discover all the new arrivals of ready-to-wear collection.
-        </p>
+    <>
+      <div className={classes.banner_container}>
+        <div className={classes.banner_content}>
+          <h1 className={classes.title}>New Collection For Fall</h1>
+          <p className={classes.para}>
+            Discover all the new arrivals of ready-to-wear collection.
+          </p>
+          <Button className={classes.btn_shop}>SHOP NOW</Button>
+        </div>
+        <figure className={classes.img_container}>
+          <img className={classes.img} src={bannerImg} alt="banner_img" />
+        </figure>
       </div>
-      <figure className={classes.img_container}>
-        <img className={classes.img} src={bannerImg} alt="banner_img" />
-      </figure>
-    </div>
+    </>
   );
 };
 
--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-import classes from "./Button.module.css";
-
 const Button = ({ children, className }) => {
-  return <button className={className || classes.btn}>{children}</button>;
+  return <button className={className}>{children}</button>;
 };
 export default Button;
