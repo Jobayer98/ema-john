9f979f9eb29fd00661af6f88eeb85bc7c72bd50b
Jobayer <jobayerrahman0220@gmail.com>
2023-03-29 14:25:17 +0600

    add cart icon on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { FaShoppingCart } from "react-icons/fa";
+
 import Hamburger from "./Hamburger";
 import logo from "../../assets/Logo.svg";
 import classes from "./Header.module.css";
@@ -11,13 +13,15 @@ const Header = () => {
         {/* brand logo */}
         <img src={logo} alt="ema-jhon-logo" />
         {/* nav list */}
-        <div>
+        <div className={classes.nav_container}>
           <div className={classes.navList}>
             <a href="">Order</a>
             <a href="">Order Review</a>
             <a href="">Manage Inventory</a>
             <a href="">Login</a>
           </div>
+          <label className={classes.quantity}>0</label>
+          <FaShoppingCart className={classes.shopping_basket} />
           <Hamburger className={classes.hamburger} />
         </div>
       </nav>
